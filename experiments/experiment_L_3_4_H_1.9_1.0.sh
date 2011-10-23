bc64fa8a196756ee6e7a0d5e6d452c33c8d4d7f0
Rohit Hiwale <rohit@rohit-laptop.(none)>
2011-10-23 18:32:10 +0530

    new exp added

---
+++ experiments/experiment_L_3_4_H_1.9_1.0.sh
@@ -0,0 +1,22 @@
+#python ../gen.py -q -M=3 -I=10 -K=3 -L=3 -N=1000 -H=1.9 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+#python ../gen.py -q -M=3 -I=10 -K=3 -L=3 -N=1000 -H=1.8 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+#python ../gen.py -q -M=3 -I=10 -K=3 -L=3 -N=1000 -H=1.7 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+#python ../gen.py -q -M=3 -I=10 -K=3 -L=3 -N=1000 -H=1.6 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+#python ../gen.py -q -M=3 -I=10 -K=3 -L=3 -N=1000 -H=1.5 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+#python ../gen.py -q -M=3 -I=10 -K=3 -L=3 -N=1000 -H=1.4 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+#python ../gen.py -q -M=3 -I=10 -K=3 -L=3 -N=1000 -H=1.3 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=3 -N=1000 -H=1.2 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=3 -N=1000 -H=1.1 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=3 -N=1000 -H=1.0 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+
+python ../gen.py -q -M=3 -I=10 -K=3 -L=4 -N=1000 -H=1.9 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=4 -N=1000 -H=1.8 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=4 -N=1000 -H=1.7 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=4 -N=1000 -H=1.6 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=4 -N=1000 -H=1.5 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=4 -N=1000 -H=1.4 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=4 -N=1000 -H=1.3 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=4 -N=1000 -H=1.2 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=4 -N=1000 -H=1.1 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+python ../gen.py -q -M=3 -I=10 -K=3 -L=4 -N=1000 -H=1.0 -res=/home/rohit/nc_experiments/experiment_L_3_4_H_1.9_1.0.results
+
