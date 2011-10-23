30d209902c0d4998acb162f3ed6d1ad13341a03d
Rohit Hiwale <rohit.hiwale@gmail.com>
2011-10-23 18:02:35 +0530

    an experiment script added

---
+++ experiment_L_5_6_H_7_2.sh
@@ -0,0 +1,13 @@
+python gen.py -q -M=3 -I=10 -K=3 -L=5 -N=1000 -H=7 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+python gen.py -q -M=3 -I=10 -K=3 -L=5 -N=1000 -H=6 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+python gen.py -q -M=3 -I=10 -K=3 -L=5 -N=1000 -H=5 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+python gen.py -q -M=3 -I=10 -K=3 -L=5 -N=1000 -H=4 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+python gen.py -q -M=3 -I=10 -K=3 -L=5 -N=1000 -H=3 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+python gen.py -q -M=3 -I=10 -K=3 -L=5 -N=1000 -H=2 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+
+python gen.py -q -M=3 -I=10 -K=3 -L=6 -N=1000 -H=7 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+python gen.py -q -M=3 -I=10 -K=3 -L=6 -N=1000 -H=6 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+python gen.py -q -M=3 -I=10 -K=3 -L=6 -N=1000 -H=5 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+python gen.py -q -M=3 -I=10 -K=3 -L=6 -N=1000 -H=4 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+python gen.py -q -M=3 -I=10 -K=3 -L=6 -N=1000 -H=3 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
+python gen.py -q -M=3 -I=10 -K=3 -L=6 -N=1000 -H=2 -res=/home/rohit/Dropbox/Work/nc/experiment_L_5_6_H_7_2.results
