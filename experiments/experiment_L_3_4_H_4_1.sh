648426e2067c80a74fac74560c3ff10772c12007
Rohit Hiwale <rohit@rohit-laptop.(none)>
2011-10-24 13:19:56 +0530

    experiments added

---
+++ experiments/experiment_L_3_4_H_4_1.sh
@@ -0,0 +1,9 @@
+python ../gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=1 -res=/home/rohit/nc_experiments/experiment_L_7_8_H_4_1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=2 -res=/home/rohit/nc_experiments/experiment_L_7_8_H_4_1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=3 -res=/home/rohit/nc_experiments/experiment_L_7_8_H_4_1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=4 -res=/home/rohit/nc_experiments/experiment_L_7_8_H_4_1.results
+
+python ../gen.py -q -M=3 -I=5 -K=3 -L=8 -N=1000 -H=1 -res=/home/rohit/nc_experiments/experiment_L_7_8_H_4_1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=8 -N=1000 -H=2 -res=/home/rohit/nc_experiments/experiment_L_7_8_H_4_1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=8 -N=1000 -H=3 -res=/home/rohit/nc_experiments/experiment_L_7_8_H_4_1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=8 -N=1000 -H=4 -res=/home/rohit/nc_experiments/experiment_L_7_8_H_4_1.results
---
+++ experiments/experiment__L_7__H_1.7_1.5_[0.1]_1.1__.sh
@@ -0,0 +1,7 @@
+#python gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=1 -res= /home/rohit/nc_experiments/experiment__L_7_8__H_4_1__.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=1.7 -res=/home/rohit/nc_experiments/experiment__L_7__H_1.7_1.5_[0.1]_1.1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=1.5 -res=/home/rohit/nc_experiments/experiment__L_7__H_1.7_1.5_[0.1]_1.1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=1.4 -res=/home/rohit/nc_experiments/experiment__L_7__H_1.7_1.5_[0.1]_1.1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=1.3 -res=/home/rohit/nc_experiments/experiment__L_7__H_1.7_1.5_[0.1]_1.1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=1.2 -res=/home/rohit/nc_experiments/experiment__L_7__H_1.7_1.5_[0.1]_1.1.results
+python ../gen.py -q -M=3 -I=5 -K=3 -L=7 -N=1000 -H=1.1 -res=/home/rohit/nc_experiments/experiment__L_7__H_1.7_1.5_[0.1]_1.1.results
